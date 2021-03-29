609f9b74a698b6164da115b8488614047fa9dc97
dez1o <dez1o@yandex.ru>
2021-03-29 19:39:09 +0300

    add filter active challenges

--- src/main/java/ru/dezlo/room/enterprise/servicesImpl/ServiceChallengeImpl.java
+++ src/main/java/ru/dezlo/room/enterprise/servicesImpl/ServiceChallengeImpl.java
@@ -3,12 +3,15 @@ package ru.dezlo.room.enterprise.servicesImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 import ru.dezlo.room.enterprise.models.ModelChallenge;
 import ru.dezlo.room.enterprise.repos.RepoChallenge;
 import ru.dezlo.room.enterprise.services.ServiceChallenge;
 import ru.dezlo.room.utils.DateUtils;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -21,7 +24,13 @@ public class ServiceChallengeImpl implements ServiceChallenge {
     @Override
     public List<ModelChallenge> getChallenges() {
         List<ModelChallenge> list = repoChallenge.findAll();
-
-        return list;
+        List<ModelChallenge> result = new ArrayList<>();
+                list.forEach(elem -> {
+            if (new Date().before(elem.getTimeInterval())) {
+                result.add(elem);
+            }
+                }
+        );
+        return result;
     }
 }
